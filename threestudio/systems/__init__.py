65c278631a85e20fc25617485215c21fbd9e83e9
theEricMa <zm2354@columbia.edu>
2024-01-04 11:54:07 +0000

    basic_upload

---
+++ threestudio/systems/__init__.py
@@ -0,0 +1,12 @@
+from . import (
+    dreamfusion,
+    fantasia3d,
+    imagedreamfusion,
+    magic3d,
+    magic123,
+    prolificdreamer,
+    sjc,
+    zero123,
+    zero123_simple,
+    mvdream
+)

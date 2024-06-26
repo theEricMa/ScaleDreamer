eabb27b97206c76046239e25e3a45ce91c5ecb55
theEricMa <zm2354@columbia.edu>
2024-06-26 21:58:09 +0800

    update

--- scripts/single-prompt-benchmark/asd_sd_nerf_long.sh
+++
@@ -1,5 +0,0 @@
-python launch.py \
-    --config configs/single-prompt_benchmark/asd_sd_nerf_long.yaml \
-    --train \
-    --gpu 0 \
-    system.prompt_processor.prompt="A DSLR photo of a peacok on a surfboard."

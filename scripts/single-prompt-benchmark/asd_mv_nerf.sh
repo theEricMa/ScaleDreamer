4a22ab0bcc3c1df165de1c0d31f143b316b55ce6
theEricMa <zm2354@columbia.edu>
2024-06-26 22:07:11 +0800

    update

---
+++ scripts/single-prompt-benchmark/asd_mv_nerf.sh
@@ -0,0 +1,5 @@
+python launch.py \
+    --config configs/single-prompt_benchmark/asd_mv_nerf.yaml \
+    --train \
+    --gpu 0 \
+    system.prompt_processor.prompt="A DSLR photo of a peacok on a surfboard."
